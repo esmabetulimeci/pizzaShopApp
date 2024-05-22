46b78d64184d935775c441b5c558df1968189539
esmauzun <esmauzun@DESKTOP-5CEU30S>
2024-05-22 13:02:58 +0300

    Add project files.

---
+++ Domain/Model/OrderAggregate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public class OrderAggregate
+    {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public double TotalAmount { get; set; }
+        public double DiscountAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerName { get; set; }
+        public virtual List<ProductAggregate> Products { get; set; }
+    }
+}
