c39727d15eb493b85ea36d23fab0c4c8d0c9787a
esmauzun <esmauzun@DESKTOP-5CEU30S>
2024-05-22 14:55:29 +0300

    feat : Commands and queries of the product object have been created.

---
+++ Application/Product/Commands/UpdateProductCommand.cs
@@ -0,0 +1,62 @@
+﻿using Application.Common.Interfaces;
+using Domain.Model;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Product.Commands
+{
+    public class UpdateProductCommand : IRequest<ProductAggregate>
+    {
+        public UpdateProductCommand(int id, string productName, string description, double price, List<Ingredients> ingredients, double quantity)
+        {
+            Id = id;
+            Name = productName;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+            Ingredients = ingredients;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public List<Ingredients> Ingredients { get; set; }
+        public double Quantity { get; set; }
+
+        public class Handler : IRequestHandler<UpdateProductCommand, ProductAggregate>
+        {
+            private IMediator _mediator;
+            private readonly IPizzaShopAppDbContext _dbContext;
+
+            public Handler(IPizzaShopAppDbContext dbContext, IMediator mediator)
+            {
+                _dbContext = dbContext;
+                _mediator = mediator;
+            }
+
+            public async Task<ProductAggregate> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+            {
+                if (string.IsNullOrEmpty(request.Name))
+                {
+                    throw new Exception("PRODUCT_NAME_CANNOT_BE_EMPTY");
+                }
+
+                var product = await _dbContext.Products.FindAsync(request.Id);
+                if (product == null)
+                {
+                    throw new Exception("PRODUCT_NOT_FOUND");
+                }
+
+                product.Update(request.Name, request.Description, request.Price, request.Ingredients, request.Quantity);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+
+                return product;
+            }
+        }
+
+    }
+}
---
+++ Application/Product/Queries/GetProductQuery.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Domain.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Product.Queries
+{
+    public class GetProductQuery : IRequest<IEnumerable<ProductAggregate>>
+    {
+
+        public class Handler : IRequestHandler<GetProductQuery, IEnumerable<ProductAggregate>>
+        {
+            private readonly IPizzaShopAppDbContext _dbContext;
+
+            public Handler(IPizzaShopAppDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+            public async Task<IEnumerable<ProductAggregate>> Handle(GetProductQuery request, CancellationToken cancellationToken)
+            {
+                var products = await _dbContext.Products.ToListAsync();
+                if(products == null)
+                {
+                    throw new Exception("NO_PRODUCTS_FOUND");
+                }
+                return products;
+            }
+        }
+      
+    }
+}
