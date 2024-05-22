5083db7dd32ff19c3fb4beb81fb3a20127d33043
esmauzun <esmauzun@DESKTOP-5CEU30S>
2024-05-22 14:54:17 +0300

    feat : context and migration processes have been completed.

---
+++ Infrastructure/PizzaShopAppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+using Domain.Model;
+using Infrastructure.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class PizzaShopAppDbContext: DbContext, IPizzaShopAppDbContext
+    {
+        public DbSet<ProductAggregate> Products { get; set; }
+        public DbSet<OrderAggregate> Orders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new NpgsqlDataSourceBuilder("Server=localhost;Port=5432;Database=pizzaShopApp;Userid=postgres;Password=123123;Include Error Detail=True;");
+            builder.EnableDynamicJson();
+            var dataSource = builder.Build();
+            optionsBuilder.UseNpgsql(dataSource);
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
