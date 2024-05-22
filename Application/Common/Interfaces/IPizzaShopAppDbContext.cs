eadbe3d2f442df0434a183a3a49aafc5496e05c2
esmauzun <esmauzun@DESKTOP-5CEU30S>
2024-05-22 14:52:39 +0300

    feat: The interface database connection has been created.

---
+++ Application/Common/Interfaces/IPizzaShopAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IPizzaShopAppDbContext
+    {
+        DbSet<ProductAggregate> Products { get; set; }
+        DbSet<OrderAggregate> Orders { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
